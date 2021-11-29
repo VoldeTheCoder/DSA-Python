0ba3e70ff9f87fd5036bc3ff7304061545cc2982
StarBoyAbhi <abhi@StarBoy.com>
2021-11-29 09:03:23 +0530

    made a bubble sort program

---
+++ bubblesort.py
@@ -0,0 +1,16 @@
+def swap(x, y):
+    temp = x
+    x = y
+    y = temp
+    return x, y
+
+def bubble_sort(arr):
+    last_index = len(arr) - 1
+    while last_index != 0:
+        for i in range(last_index):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = swap(arr[i], arr[i+1])
+        last_index-=1
+    print(arr)
+
+bubble_sort([21, 4, 1, 3, 9, 20, 25, 6, 21, 14])
